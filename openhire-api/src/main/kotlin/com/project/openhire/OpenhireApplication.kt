e97e0868783ce48cf303c4654c09cc87f9854c53
khkil <robot625@naver.com>
2025-02-07 15:54:04 +0900

    feat: api 프로젝트 셋팅

---
+++ openhire-api/src/main/kotlin/com/project/openhire/OpenhireApplication.kt
@@ -0,0 +1,11 @@
+package com.project.openhire
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpenhireApplication
+
+fun main(args: Array<String>) {
+	runApplication<OpenhireApplication>(*args)
+}
---
+++ openhire-api/src/test/kotlin/com/project/openhire/OpenhireApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.project.openhire
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OpenhireApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
